f5b0ba6be6098de8e64baa62ed02caaf3c6686e6
Gerald <geraldcelanji@gmail.com>
2022-08-12 20:01:48 +0200

    Added update functionality to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,37 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    #render plain: params[:article]
+    #render plain: @article.inspect
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article # same with below line
+      #redirect_to article_path(@article) #extracts the id and pass it as a argument
+    else
+      render 'new'
+    end
+  end
+
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 end
