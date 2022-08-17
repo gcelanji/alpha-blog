c79336d707676c0d5a781cf02ab41d3eafc78199
Gerald <geraldcelanji@gmail.com>
2022-08-17 13:51:36 +0200

    Cleanup layout

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Get rid of div error tags that are being add by Rails automatically when the form hits a validation issue
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
