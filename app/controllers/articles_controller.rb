7ee024d76a917efb88282ef1bd665cae91e7dd1f
Gerald <geraldcelanji@gmail.com>
2022-08-22 11:41:24 +0100

    Add admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,7 +58,7 @@ before_action :require_same_user, only:[:edit, :update, :destroy]
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user && !current_user.admin?
       flash[:alert] = "Unauthorized access. You can edit or delete your own articles only"
       redirect_to @article
     end
